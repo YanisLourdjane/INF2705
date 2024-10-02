8c96a2f6f43b035178423fa402f968f9be79bfe7
YanisLourdjane <yanis.lourdjane@polymtl.ca>
2024-10-02 18:34:37 -0400

    start texture.cpp

--- tp2-etudiant/src/texture.cpp
+++ tp2-etudiant/src/texture.cpp
@@ -15,6 +15,15 @@ Texture2D::Texture2D(const char* path)
         std::cout << "Error loading texture \"" << path << "\": " << stbi_failure_reason() << std::endl;
 
     // TODO - Chargement de la texture
+    GLenum format = GL_RGB;
+    if (nChannels == 1)
+        format = GL_RED;
+    else if (nChannels == 3)
+        format = GL_RGB;
+    else if (nChannels == 4)
+        format = GL_RGBA;
+        
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
 
     stbi_image_free(data);
 }
@@ -22,21 +31,30 @@ Texture2D::Texture2D(const char* path)
 Texture2D::~Texture2D()
 {
     // TODO
+    glDeleteTextures(1, &m_id);
 }
 
 void Texture2D::setFiltering(GLenum filteringMode)
 {
     // TODO - min et mag filter
+    glBindTexture(GL_TEXTURE_2D, m_id);
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filteringMode);
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filteringMode );
 }
 
 void Texture2D::setWrap(GLenum wrapMode)
 {
     // TODO
+    glBindTexture(GL_TEXTURE_2D, m_id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapMode);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapMode);
 }
 
 void Texture2D::enableMipmap()
 {
     // TODO - mipmap et filtering correspondant
+    glBindTexture(GL_TEXTURE_2D, m_id);
+    glGenerateMipmap( GL_TEXTURE_2D );
 }
 
 void Texture2D::use()
